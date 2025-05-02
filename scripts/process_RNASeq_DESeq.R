734d388aed007ffa988128a66bb52a3b08ac01b3
Jason Stajich <jasonstajich.phd@gmail.com>
2025-05-02 10:32:43 -0700

    functions for pairwise

--- scripts/process_RNASeq_DESeq.R
+++ scripts/process_RNASeq_DESeq.R
@@ -125,9 +125,9 @@ p<- ggplot(pcaData, aes(PC1, PC2, color=genotype,shape=treatment,label=treatment
 ggsave("plots/PCA_expression.pdf",p)
 
 # only look at one treatment (growth condition) at a time contrast genotypes
-calcPlotPairwise <- function(c) {
+calcPlotPairwiseCondition <- function(c) {
   
-  sampleTablePair = sampleTable %>% filter(condition == c)
+  sampleTablePair = sampleTable %>% dplyr::filter(condition == c)
   print(sampleTablePair)
   pdf(sprintf("plots/RNASeq_%s_contrastGenotype.pdf",c))
   genes = rownames(countdata)
@@ -138,6 +138,135 @@ calcPlotPairwise <- function(c) {
   dds <- DESeqDataSetFromMatrix(countData = countdataPair,
                                 colData   = sampleTablePair, 
                                 design    = ~ genotype )
+
+  nrow(dds)
+  dds <- dds[ rowSums(counts(dds)) > 1, ]
+  nrow(dds)
+
+  dds <- estimateSizeFactors(dds)
+  dds <- estimateDispersions(dds)
+
+  vsd <- vst(dds, blind=FALSE)
+  rld <- rlog(dds, blind=FALSE)
+  head(assay(vsd), 3)
+  head(assay(rld), 3)
+
+  df <- bind_rows(
+      as.data.frame(log2(counts(dds, normalized=TRUE)[, 1:2]+1)) %>%
+        mutate(transformation = "log2(x + 1)"),
+      as.data.frame(assay(rld)[, 1:2]) %>% mutate(transformation = "rlog"),
+      as.data.frame(assay(vsd)[, 1:2]) %>% mutate(transformation = "vst")
+      )
+    
+
+  colnames(df)[1:2] <- c("x", "y")
+  
+  select <- order(rowMeans(counts(ddsAll,normalized=TRUE)),
+                  decreasing=TRUE)[1:50]
+
+  MA.idx = t(combn(1:4, 2))
+  for( i in  seq_along( MA.idx[,1])){ 
+    MDPlot(counts(dds, normalized = T), 
+          c(MA.idx[i,1],MA.idx[i,2]), 
+          main = paste( colnames(dds)[MA.idx[i,1]], " vs ",
+                         colnames(dds)[MA.idx[i,2]] ), ylim = c(-3,3))
+  }
+  
+  ggplot(df, aes(x = x, y = y)) + geom_hex(bins = 80) +
+    coord_fixed() + facet_grid( . ~ transformation)
+  
+  select <- order(rowMeans(counts(dds,normalized=TRUE)),
+                  decreasing=TRUE)[1:50]
+  
+  #datCollapsed <- collapseReplicates(dds, groupby=dds$genotype,run=dds$replicate,renameCols=TRUE)
+  
+  df2 <- as.data.frame(colData(dds)[,c("condition","genotype")])
+  rownames(df2) = colnames(countdataPair)
+  colnames(df2) = c("Treatment", "Genotype")
+  pheatmap(assay(vsd)[select,], cluster_rows=FALSE, show_rownames=TRUE,
+           fontsize_row = 7,fontsize_col = 7,
+           cluster_cols=FALSE, annotation_col=df2,main="VSD Top Expression")
+  
+  topVar <- head(order(rowVars(assay(vsd)),
+                       decreasing=TRUE),60)
+  mat  <- assay(vsd)[ topVar, ]
+  
+  pheatmap(mat, show_rownames=TRUE,
+           fontsize_row = 7,fontsize_col = 7,
+           cluster_cols=FALSE, annotation_col=df2,main="VSD Most different")
+  
+  pheatmap(assay(rld)[select,], cluster_rows=FALSE, show_rownames=TRUE,
+           fontsize_row = 7,fontsize_col = 7,
+           cluster_cols=FALSE, annotation_col=df2,main="RLD Top Expression")
+  
+  pheatmap(assay(rld)[select,], cluster_rows=TRUE, show_rownames=TRUE,
+           fontsize_row = 7,fontsize_col = 7,
+           cluster_cols=FALSE, annotation_col=df2,main="RLD Top Expression")
+  
+  topVar <- head(order(rowVars(assay(rld)),
+                       decreasing=TRUE),60)
+  mat  <- assay(rld)[ topVar, ]
+  mat  <- mat - rowMeans(mat)
+  pheatmap(mat, show_rownames=TRUE,
+           fontsize_row = 7,fontsize_col = 7,
+           cluster_cols=FALSE, annotation_col=df2,main="RLD Most different")
+  
+  norm.counts <- counts(dds, normalized=TRUE)
+  log.norm.counts <- log2(norm.counts + 1)
+  
+  topVarGenes <- order(-rowVars(log.norm.counts)[0:100])
+  mat<-log.norm.counts[topVarGenes,]
+  mat<-mat -rowMeans(mat)
+  
+  pheatmap(mat,method="complete",main = "TopVar normalized", show_rownames = F,
+          annotation_legend = FALSE, legend=T, cluster_rows=TRUE, cexRow=0.55 )
+  
+  topVarGenes1 <- order(-rowVars(assay(rld)))[0:1000]
+  mat1 <- assay(rld)[ topVarGenes1, ]
+  mat1<- mat1 - rowMeans(mat1)
+  
+  pheatmap(mat1, method="complete",
+           main = "Unsupervised 1000 genes ",
+           show_rownames = F,annotation_legend = FALSE, 
+           legend=T, cluster_cols=TRUE)
+  
+  dds <- DESeq(dds)
+  res <- results(dds)
+  res    
+  #res <- results(dds, contrast=c("genotype","AF293","CEA10"))
+  #res
+  
+  resultsNames(dds)
+  resLFC <- lfcShrink(dds, coef=resultsNames(dds)[2], type="apeglm")
+  resLFC
+  summary(resLFC)
+  summary(res)
+  res05 <- results(dds,alpha=0.05)
+  summary(res05)
+  sum(res$padj < 0.01, na.rm=TRUE)
+  plotMA(res, ylim=c(-2,2),main=sprintf("%s",resultsNames(dds)[2]))
+  plotMA(resLFC, ylim=c(-2,2),main=sprintf("%s",resultsNames(dds)[2]))
+  
+  resSig <- subset(resLFC, padj < 0.05)
+  resSig <- resSig[order(resSig$pvalue),]
+  write.csv(resSig,sprintf("reports/%s_%s.csv",c,resultsNames(dds)[2]))
+  write.csv(fpm(dds),sprintf("reports/%s_allGeno_FPM.csv",c))
+  dev.off()
+}
+
+# only look at one genotype (strain) at a time contrast conditions
+calcPlotPairwiseGenotype <- function(g) {
+  sampleTablePair = sampleTable %>% dplyr::filter(genotype == g)
+  print(sampleTablePair)
+  pdf(sprintf("plots/RNASeq_%s_contrastCondition.pdf",g))
+  genes = rownames(countdata)
+  countdataPair <- as.data.frame(as_tibble(countdata) %>% dplyr::select(starts_with(sprintf('%s_',g))))
+  
+  rownames(countdataPair) <- genes
+  
+  dds <- DESeqDataSetFromMatrix(countData = countdataPair,
+                                colData   = sampleTablePair, 
+                                design    = ~ condition )
 
   nrow(dds)
   dds <- dds[ rowSums(counts(dds)) > 1, ]
@@ -232,32 +361,31 @@ calcPlotPairwise <- function(c) {
            show_rownames = F,annotation_legend = FALSE, 
            legend=T, cluster_cols=TRUE)
   
-  # need to re-isolate this to test for Genotype variation too
-  
   dds <- DESeq(dds)
   res <- results(dds)
   res    
-  res <- results(dds, contrast=c("genotype","AF293","CEA10"))
-  res
+  #res <- results(dds, contrast=c("treatment","AF293","CEA10"))
+  #res
   
   resultsNames(dds)
-  resLFC <- lfcShrink(dds, coef="genotype_CEA10_vs_AF293", type="apeglm")
+  resLFC <- lfcShrink(dds, coef=resultsNames(dds)[2], type="apeglm")
   resLFC
   summary(resLFC)
   summary(res)
   res05 <- results(dds,alpha=0.05)
   summary(res05)
   sum(res$padj < 0.01, na.rm=TRUE)
-  plotMA(res, ylim=c(-2,2),main=sprintf("%s CEA10 vs AF293",c))
-  plotMA(resLFC, ylim=c(-2,2),main=sprintf("%s CEA10 vs AF293",c))
+  plotMA(res, ylim=c(-2,2),main=sprintf("%s",resultsNames(dds)[2]))
+  plotMA(resLFC, ylim=c(-2,2),main=sprintf("%s",resultsNames(dds)[2]))
   
   resSig <- subset(resLFC, padj < 0.05)
   resSig <- resSig[order(resSig$pvalue),]
-  write.csv(resSig,sprintf("reports/%s_CEA10_vs_AF293.csv",c))
-  write.csv(fpm(dds),sprintf("reports/%s_allgeno_FPM.csv",c))
+  write.csv(resSig,sprintf("reports/%s_%s.csv",g,resultsNames(dds)[2]))
+  write.csv(fpm(dds),sprintf("reports/%s_allConditions_FPM.csv",g))
   dev.off()
 }
-lapply(unique(treatment),calcPlotPairwise)
+lapply(unique(sampleTable$genotype),calcPlotPairwiseGenotype)
+lapply(unique(sampleTable$condition),calcPlotPairwiseCondition)
 
 
 
